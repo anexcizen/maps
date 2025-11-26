debe861f52e1279d1b21e5f528f1ec96380c0355
anita <anitatalbieva@gmail.com>
2025-11-27 01:23:25 +0500

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import ru.transport.Transport;
+
+public class Main {
+    public static void main(String[] args) {
+        Transport transport = new Transport("автобус", 10);
+        System.out.println(transport);
+    }
+}
---
+++ src/main/java/ru/transport/Transport.java
@@ -0,0 +1,23 @@
+package ru.transport;
+
+public class Transport {
+    protected String name;
+    protected int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Transport(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String toString() {
+        return "транспорт: " + name;
+    }
+}
