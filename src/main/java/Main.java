bc8e46394d83143b9d42235a95df0cbd26bbe186
anita <anitatalbieva@gmail.com>
2025-11-27 02:44:47 +0500

    main приведен в рабочее состояние

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,9 @@
 import ru.route.*;
 import ru.transport.*;
+import ru.service.*;
 
 public class Main {
     public static void main(String[] args) {
-        Train train = new Train();
-        Plane plane = new Plane();
-        Bus bus = new Bus();
-        Car car = new Car();
-        Route route = new Route("Казань", "Москва", "11:00", "19:11", 839, plane);
-        System.out.println(route);
+        InputRoute.checkRoute();
     }
 }
